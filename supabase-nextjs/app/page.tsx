79d485308d4a5e4a87514cdad67647a7120289a0
reecevela <reecevela@outlook.com>
2024-12-15 15:56:04 -0600

    basics

--- supabase-nextjs/app/page.tsx
+++ supabase-nextjs/app/page.tsx
@@ -4,6 +4,12 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <a href="./login" className="flex items-center gap-2">
+          Login
+        </a>
+        <a href="./signup" className="flex items-center gap-2">
+          Signup
+        </a>
         <Image
           className="dark:invert"
           src="/next.svg"
